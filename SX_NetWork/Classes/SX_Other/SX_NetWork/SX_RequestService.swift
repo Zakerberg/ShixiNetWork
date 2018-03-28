1c4b348d84474906cdf844d7da4d68aafbf31352
Zakerberg <blmichael1129@icloud.com>
2018-03-28 17:45:18 +0800

    RequestService

---
+++ SX_NetWork/Classes/SX_Other/SX_NetWork/SX_RequestService.swift
@@ -0,0 +1,55 @@
+//
+//  SX_RequestService.swift
+//  SX_NetWork
+//
+//  Created by Michael 柏 on 3/28/18.
+//  Copyright © 2018 Shixi (Beijing) Technology Limited (Company). All rights reserved.
+//
+
+import UIKit
+
+class SX_RequestService: NSObject {
+    
+    /// 请求成功的Handler
+    typealias SXRequestSuccessHandler = ((_ responseData: Any) -> ())
+    /// 请求失败的Handler
+    typealias SXRequestFailureHandler = ((_ error: NSError) -> ())
+    
+    /**
+     *  设置请求头
+     *
+     *  param headerKV  请求头Key-Value
+     */
+    class func setHeader (headerKV: [NSString : NSString]) {
+        
+        
+        
+        
+    }
+}
+
+
+
+//MARK: - GET
+extension SX_RequestService {
+    
+    
+    
+    
+    
+}
+
+//MARK: - POST
+extension SX_RequestService {
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
